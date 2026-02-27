2bdd96fa7b7487a3320f002503f57d1ae65e86f1
123ishrat123 <ishratattria1@gmail.com>
2026-02-27 14:11:24 +0500

    conditional statements

---
+++ statements conditonal.py
@@ -0,0 +1,17 @@
+print(5>6)
+print(5<6)
+print(5==6)
+print(5!=6)
+print(5>=6)
+print(5<=6)
+print(5>5)
+print(5>=5)
+
+#condition statement
+if 5>6:
+    print("5 is greater then 6")
+
+if 5<6:
+    print("5 is less then 6")
+if 5==6:
+    print("5 is equal to 6")
