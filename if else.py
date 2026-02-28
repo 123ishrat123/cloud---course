17a24d7dd3b0ecd5cf8acaa59874f4122f92a638
123ishrat123 <ishratattria1@gmail.com>
2026-02-28 15:48:15 +0500

    if else statements and exammple

---
+++ if else.py
@@ -0,0 +1,9 @@
+#age = int(input("enter your age"))
+#if age>18:
+  #  print("you are eligible for vote")
+
+marks =int(input("enter student marks"))
+if marks> 75:
+    print("you are pass with grade A", marks)
+else:
+    print("you are fail")
--- statements conditonal.py
+++ statements conditonal.py
@@ -15,3 +15,11 @@ if 5<6:
     print("5 is less then 6")
 if 5==6:
     print("5 is equal to 6")
+
+marks = 85
+if marks>=80:
+    print("Grade A")
+
+marks =int(input("enter your marks:"))
+if marks>50:
+    print("you are pass",marks)
