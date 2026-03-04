b3a57eb7dd563f0d304765ffa7486b3b169508cd
123ishrat123 <ishratattria1@gmail.com>
2026-03-04 15:51:34 +0500

    if elif else program

---
+++ if elif else.py
@@ -0,0 +1,17 @@
+day_number =int(input("enter day number of your choice"))
+if day_number == 1:
+    print(" The day is Monday")
+elif day_number == 2:
+    print(" The day is Tuesday")
+elif day_number ==3:
+     print(" The day is Wednesday")
+elif day_number == 4:
+      print("The day is Thursday")
+elif day_number == 5:
+     print(" The day is Friday")
+elif day_number ==6:
+   print("The day is Saturday")
+elif day_number ==7:
+ print("The day is Sunday")
+else:
+    print("please enter valid day number")
