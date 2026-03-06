78d3702a19061d3d6cb73d2acd023bbe86841b8c
123ishrat123 <ishratattria1@gmail.com>
2026-03-06 14:40:58 +0500

    addation file

---
+++ add .py
@@ -0,0 +1,20 @@
+num1 =12
+num2 =25
+add =num1+num2
+print(add)
+print(add)
+
+
+
+
+variable1 = 45
+variable2 =10
+result = variable1 + variable2
+print(result)
+
+
+
+a = 98
+b =45
+c = a-b
+print(c)
